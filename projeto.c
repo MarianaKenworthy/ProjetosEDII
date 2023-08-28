cf8ac6762d234206d2f2b1d330d55fb90542b958
OdairNeto2004 <odair.neto@unesp.br>
2023-08-28 19:42:11 -0300

    Bug fixes removeReg

--- projeto.c
+++ projeto.c
@@ -1,63 +1,42 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
-/* to-do
-- onde parou no arquivo cadastro, insere, e remove
-- first-fit do insere
-- compactação (quando tudo estiver pronto)
-- deixar bonitinho (🏳‍🌈)
+/* To-do:
+- Onde parou no arquivo cadastro, insere, e remove;
+- First-fit do insere;
+- Compactação (quando tudo estiver pronto);
+- Deixar bonitinho (🏳‍🌈);
 */
 
-typedef struct segurado{
+typedef struct segurado
+{
     char codigo[4];
     char nome[50];
     char seguradora[50];
     char tipo[30];
 } seg;
 
-struct pilha{
-    int info;
-    struct pilha *prox;
-};
-
-void push (int x, struct pilha **topo){
-    struct pilha *aux;
-    aux = (struct pilha *)malloc(sizeof(struct pilha));
-    aux->info = x;
-    aux->prox = *topo;
-    *topo = aux;
-}
-
-void pop (struct pilha **p, int *x){
-    struct pilha *aux;
-    if(*p){
-        *x = (*p)->info;
-        aux = *p;
-        *p = aux->prox;
-        free(aux);
-    }else{
-        printf("vazia");
-    }
-}
-
-void montaCabecalho (FILE* output){
+void montaCabecalho(FILE *output)
+{
     char tam;
     char registro[4];
     sprintf(registro, "**|");
-    //assim ficaria {3**|}, onde:
-    //*indica q é removido pra n ler em outros momentos, mas no caso é o cabeçalho dos removidos
-    //*indica q por enquanto nenhum foi removido, porém vai mudar pra distância do mais recentemente removido
-    //|indica o começo do arquivo de verdade, foi um jeito mais fácil de arrumar esse problema, vai ter q mudar algumas coisas
+    // Assim ficaria {3**|}, onde:
+    // '*' Indica que é removido para não ler em outros momentos, mas no caso é o cabeçalho dos removidos
+    // '*' Indica que por enquanto nenhum foi removido, porém vai mudar para a distância do mais recentemente removido
+    // '|' Indica o começo do arquivo de verdade, foi um jeito mais fácil de arrumar esse problema, levar isso em conta nas funções
     tam = strlen(registro);
 
     fwrite(&tam, sizeof(char), 1, output);
     fwrite(&registro, sizeof(char), tam, output);
 }
 
-void montaCampos (FILE* input, FILE*output){
+void montaCampos(FILE *input, FILE *output)
+{
     char tam;
-    seg segurado; 
+    seg segurado;
     fread(&segurado, sizeof(seg), 1, input);
     char registro[135];
     sprintf(registro, "%s#%s#%s#%s#", segurado.codigo, segurado.nome, segurado.seguradora, segurado.tipo);
@@ -67,113 +46,120 @@ void montaCampos (FILE* input, FILE*output){
     fwrite(&registro, sizeof(char), tam, output);
 }
 
-void removeReg (FILE* input, FILE* output, struct pilha **offset){//esse offset tá fazendo algo? (offset, pop, push, struct da pilha)
+void removeReg(FILE *input, FILE *output)
+{
     char codigo[4], aux[4], verificaRemovido;
-    fread(codigo, sizeof(char), 4, input); //le o codigo a ser removido
+    fread(codigo, sizeof(char), 4, input); // Lê o codigo a ser removido do remove.bin
 
     char contaDistancia = 4;
 
-    fseek(output, 4, 0);// garante que ele ta no começo (4 por conta do cabeçalho)
-    fread(&aux, sizeof(char), 4, output);
-    fseek(output, 4, 0);
+    fseek(output, 4, 0);                  // Garante que está no começo (4 por conta do cabeçalho)
+    fread(&aux, sizeof(char), 4, output); // Lê o código em cadastro.dat, para comparar com o remove.bin
 
-    while((codigo[0]!=aux[1])||(codigo[1]!=aux[2])||(codigo[2]!=aux[3])){
+    fseek(output, 4, 0);
+    bool primeiraRemocao = false;
+    if ((codigo[0] == aux[1]) || (codigo[1] == aux[2]) || (codigo[2] == aux[3]))
+    {
+        primeiraRemocao = true;
+        fseek(output, 1, SEEK_CUR);
+    }
+
+    while ((codigo[0] != aux[1]) || (codigo[1] != aux[2]) || (codigo[2] != aux[3]))
+    {
         contaDistancia += aux[0] + 1;
-        fseek(output, aux[0]+1, SEEK_CUR);
-        if(fread(aux, sizeof(char), 4, output) == 0){
+        fseek(output, aux[0] + 1, SEEK_CUR); // Vai para o próximo código em cadastro.dat
+        if (fread(aux, sizeof(char), 4, output) == 0)
+        {
             printf("\nCodigo nao encontrado\n");
             return;
         }
-    }
-    //volta e coloca o asterisco: 37001 -> 37*01
-    fseek(output, -3, SEEK_CUR);
-    if ((fread(&verificaRemovido, 1, 1, output)) == 1){
-        if (verificaRemovido == '*'){
-            printf("\nEsse registro ja foi removido\n");
-        }else{
-            fseek(output, -1, SEEK_CUR);
-            char asterisco = '*';
-            fwrite(&asterisco, sizeof(char), 1, output);
-
-            fseek(output, 2, 0);
-            char proximo;
-            fread(&proximo, sizeof(char), 1, output);
-            fseek(output, -1, SEEK_CUR);
-            fwrite(&contaDistancia, sizeof(char), 1, output);//no cabeçalho -> 2*87; proximo vai ser oq tava lá
-
-            fseek(output, contaDistancia+2, 0);//vai pra onde tava antes agora com o proximo feito
-            fwrite(&proximo, sizeof(char), 1, output);//coloca o próximo, se for asterisco é o último da pilha (primeiro q foi colocado)
+        else
+        {
+            if ((codigo[0] != aux[1]) || (codigo[1] != aux[2]) || (codigo[2] != aux[3]))
+                fseek(output, -4, SEEK_CUR);
         }
     }
+
+    if (!primeiraRemocao)
+        fseek(output, -3, SEEK_CUR); // Assim que achar, volta e coloca o asterisco: 37001 -> 37*01
+    char asterisco = '*';
+    fwrite(&asterisco, sizeof(char), 1, output);
+
+    fseek(output, 2, 0); // Salva o próximo da pilha ('*' se não tiver) em proximo
+    char proximo;
+    fread(&proximo, sizeof(char), 1, output);
+
+    fseek(output, -1, SEEK_CUR);
+    fwrite(&contaDistancia, sizeof(char), 1, output); // Escreve a distância do mais recentemente removido no cabeçalho
+
+    fseek(output, contaDistancia + 2, 0);      // Vai para o mais recentemente removido
+    fwrite(&proximo, sizeof(char), 1, output); // Coloca o próximo da pilha, se for asterisco é o último (primeiro que foi colocado)
 }
 
-void compacta (FILE* output){
-    //precisa lembrar de zerar a pilha aqui mas como ainda não tem pilha nao fiz tambem precisar mudar o tamanho dos registros naquele primeiro bit
-    //já que encurta eles
+void compacta(FILE *output)
+{
+    // Precisa lembrar de zerar a pilha aqui mas como ainda não tem pilha não fiz
+    // Também precisa mudar o tamanho dos registros naquele primeiro bit já que encurta eles
     fseek(output, 0, 0);
-    FILE* output2 = output;
+    FILE *output2 = output;
     int contador = 0, i;
     char ch, proximo, leitura, buffer[sizeof(seg)];
 
-    while(fread(&ch, sizeof(char), 1, output)!=0){
-        if(contador == 0)
+    while (fread(&ch, sizeof(char), 1, output) != 0)
+    {
+        if (contador == 0)
             proximo = ch;
 
-        if(ch = '#')
+        if (ch = '#')
             contador++;
 
-        if(contador==4){ //eu desenhei essa parte se tu quiser
+        if (contador == 4)
+        {
             fseek(output2, proximo, 1);
             fread(&leitura, sizeof(char), 1, output2);
             fread(buffer, sizeof(char), leitura, output2);
 
             fwrite(&leitura, sizeof(char), 1, output);
             fwrite(buffer, sizeof(char), leitura, output);
-           // fseek(output, tamanho atualizado, SEEK_CUR);
+            // fseek(output, tamanho atualizado, SEEK_CUR);
             contador = 0;
         }
-
     }
 }
 
-
-int main () {
-    FILE* output = fopen("cadastro.dat", "r+b");
+int main()
+{
+    FILE *output = fopen("cadastro.dat", "r+b");
     if (!output)
-    output = fopen("cadastro.dat", "w+b");
+        output = fopen("cadastro.dat", "w+b");
     montaCabecalho(output);
 
-    FILE* input1 = fopen("insere.bin", "rb");
-    FILE* input2 = fopen("remove.bin", "rb");
-
-    struct pilha *offset = NULL;
+    FILE *input1 = fopen("insere.bin", "rb");
+    FILE *input2 = fopen("remove.bin", "rb");
 
     char escolha;
-    do {
-    printf("O que deseja fazer?\n\n1- Inserir\n2- Remover\n3- Compactar\n4- Sair\n");
-    scanf(" %c", &escolha);
-
-        switch(escolha){
-            case '1': {
-                
-                montaCampos(input1, output);
-
+    do
+    {
+        printf("\n---------------------------\n\nO que deseja fazer?\n\n1- Inserir\n2- Remover\n3- Compactar\n4- Sair\n");
+        scanf(" %c", &escolha);
+        switch (escolha)
+        {
+        case '1':
+        {
+            montaCampos(input1, output);
             break;
-            }
-
-            case '2':{
-                
-                removeReg(input2, output, &offset);
-
-            break;
-            }
-            case '3':
-
-            break;
-
         }
-
-    } while(escolha != '4');
+        case '2':
+        {
+            removeReg(input2, output);
+            break;
+        }
+        case '3':
+        {
+            break;
+        }
+        }
+    } while (escolha != '4');
 
     fclose(input1);
     fclose(input2);
