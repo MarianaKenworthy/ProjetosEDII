94bb5cdfdba9044d6d688eeb18a26f28c8f14030
MarianaKenworthy <m.kenworthy@unesp.br>
2023-09-23 21:00:16 -0300

    structure update

---
+++ projeto2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "projeto2.h"
+
+void addIndice (FILE*);
+void buscaP;
+void buscaS;
+
---
+++ projeto2.h
@@ -0,0 +1,12 @@
+typedef struct segurado {
+    char codigo[4];
+    char nome[50];
+    char seguradora[50];
+    char tipo[30];
+} seg;
+
+typedef struct indice{
+    char chave[4];
+    int offset;
+} ind;
+
